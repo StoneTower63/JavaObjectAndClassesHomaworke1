17c831ee34b9ff39a37037dece6d162b60015ad2
Roman <Gushlenko63@mail.ru>
2026-03-12 15:04:45 +0400

    Completed homework on the topic: "Methods of Objects"

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,22 @@ public class Main {
         prorok.setYear(1828);
         System.out.println("Год публикации: " + prorok.getYear());
 
+        //ДЗ добавим копии для сверки
+        Author pushkin2 = new Author("Alexhander", "Pushkin");
+        Book prorok2 = new Book("Prorok", pushkin2, 1828);
+
+        //Сравним
+        System.out.println("Книги одинаковые? " + prorok.equals(prorok2));
+
+        //Проверим хешКоды
+        System.out.println("Хеш первой книги " + prorok.hashCode());
+        System.out.println("Хеш второй книги " + prorok2.hashCode());
+
+        //Проверим вывод
+        System.out.println("Инфа о первой книге: " + prorok.toString());
+        System.out.println("Инфа о второй книге: " + prorok2.toString());
+        //Конечная
+
         Author dostoevsky = new Author("Fyodor", "Dostoevsky");
         Book prestuplenieINakazanie = new Book("Prestyplenie i nakazanie", dostoevsky, 1999);
         prestuplenieINakazanie.setYear(1866);
